5ee7c29d7b8512683b54953cf854187ad12466d9
anandhbala <65329542+anandhbala@users.noreply.github.com>
2020-06-19 11:55:53 +0530

    Update deletescript.sh

--- deletescript.sh
+++ deletescript.sh
@@ -1,1 +1,9 @@
-pm2 delete cl-frontend
+#!/bin/bash
+sudo pm2 describe cl-frontend > /dev/null
+RUNNING=$?
+
+if [ "${RUNNING}" -eq 0 ]; then
+ sudo -E pm2 delete cl-frontend
+else
+  sudo pm2 list
+fi;
